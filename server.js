666f03b89449c324bde9ac394f54f70f12f4e4bf
trenton-telge <darkforce102@gmail.com>
2021-07-30 12:43:40 -0500

    Changes for heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
-app.set('port', 5000);
+//app.set('port', 5000);
 app.use('/', express.static(__dirname + '/static'));
 
 // Routing
